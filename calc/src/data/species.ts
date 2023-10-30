37846131e7c6c419efd5a87e59b2e0387d0ad2c3
DarkShinyGiratina <williamsross20@gmail.com>
2023-10-30 18:08:03 -0400

    zorua incident

--- calc/src/data/species.ts
+++ calc/src/data/species.ts
@@ -8611,7 +8611,7 @@ const SM_PATCH: { [name: string]: DeepPartial<SpeciesData> } = {
   },
   Zorua: {
     types: ["Dark"],
-    bs: { hp: 50, at: 65, df: 50, sa: 86, sd: 50, sp: 90 },
+    bs: { hp: 50, at: 65, df: 50, sa: 85, sd: 50, sp: 90 },
   },
   Zoroark: {
     types: ["Dark"],
